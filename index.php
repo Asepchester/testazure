e82c3c8d4295d52f55830e19bcbd34a6c377cc99
Asepchester <35957079+Asepchester@users.noreply.github.com>
2019-06-18 14:59:13 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "dicodingasserver";
+    $user = "dicoding";
+    $pass = "Abcde12345";
+    $db = "dicodingdb";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
